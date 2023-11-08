402629a0e5ca8769e8d26bc92e60060db44373a3
Olingo <195367@ecam.be>
2023-11-08 12:37:13 +0100

    aled

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
+let router = require('./routes');
+const db = require('./db');
+app.use(express.urlencoded({extended: true}));
+
+
+app.use(express.json());
+app.use(express.static("express"));
+// default URL for website
+app.use('/', router)
+
+const server = http.createServer(app);
+const port = 80;
+server.listen(port);
+console.debug('Server listening on port ' + port);
